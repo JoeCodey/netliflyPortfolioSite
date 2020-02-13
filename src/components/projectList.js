c7735c9661151326dca2a98fd4894489309771be
Joseph lefebvre <jlefe074@uottawa.ca>
2020-02-13 15:11:20 -0500

    Added priority sorting for porject list. Controlled by regular expressions evaluated on reposiitory names

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react' ; 
+import React, {Component} from 'react' ;  
 import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
 const fs = require('fs') ;
 
@@ -55,31 +55,7 @@ class ProjectList extends React.Component{
                 <div class="w-100">
                   <h2 class="mb-5">Projects</h2>
 
-                  <Get url={baseURL+`/users/${gitUsername}/repos?sort=updated`}>
-                            {(error, response, isLoading, makeRequest, axios) => {
-                    if(error) {
-                        return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
-                    }
-                    else if(isLoading) {
-                        return (<div>Loading...</div>)
-                    }
-                    else if(response !== null) {
-                        {console.log(response.data)}
-
-                        for (let i = 0 ; i < response.data.length ; i++){
-                            response.data[i] = <IndivProject language={response.data[i].language}
-                            name={response.data[i].name} 
-                            description={response.data[i].description} 
-                            svn_url={response.data[i].svn_url}  
-                             /> ;
-                        }
-                        return (<div> {response.data} </div>)
-                    }
-                    return (<div>Default message before request is made.</div>)
-                    }}
-                  </Get>
-           
-               {/** creating indiviual instances of indivProject components */} 
+                {githubAPI_GET_Projects(baseURL,gitUsername)}
                     
               </div>
 
@@ -89,6 +65,80 @@ class ProjectList extends React.Component{
         );
         }
     
+
+}
+
+function githubAPI_GET_Projects(githubAPI_URL, username){ 
+        {/** Asynchonous GET request via react-axios library which fetches most recent project list from gitAPI */}
+            let priority_projects = {
+                    arduinoprojects : /(A|a)+rdu/ ,
+                    dataScience : / *D|date/ ,
+                    honours: /Vehicle/ 
+                };
+
+        let projectList_array = [] ; 
+
+        return(
+        <Get url={githubAPI_URL+`/users/${username}/repos?sort=updated`}>
+                    {(error, response, isLoading, makeRequest, axios) => {
+                if(error) {
+                return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
+                }
+                else if(isLoading) {
+                return (<div>Loading...</div>)
+                }
+                else if(response !== null) {
+                {console.log(response.data)}
+
+                {/**
+                    Push projects with arduino and datascience in repositiory titile 
+                    to the top of the project list.     
+                */}
+                
+                for(let i=0 ; i <response.data.length ; i++){
+                    console.log(`${response.data[i].name} : ${priority_projects.arduinoprojects.test(response.data[i].name)}` );
+
+                }
+            
+
+                response.data.sort( (repo1,repo2)=> ( priority_projects.arduinoprojects.test(repo1.name) || priority_projects.honours.test(repo1.name) || priority_projects.dataScience.test(repo1.name) ? -1: 1)) ;     
+                
+                
+                // response.data.sort( (repo1,repo2)=> {
+                    
+                //     if( /C\+\+/.test(repo1.language) && /C\+\+/.test(repo2.language)){
+                //          if(Date.parse(repo1.updated_at) > Date.parse(repo2.updated_at) ? -1 : 1 ); 
+                //     } 
+                // } ) ; 
+
+
+                // response.data.sort( (repo1,repo2)=> {
+                //     if( /C\+\+/.test(repo1.language) && /C\+\+/.test(repo2.language)){
+                //         if( Date.parse(repo1.updated_at) > Date.parse(repo2.updated_at) ? 1 : -1 ) ; 
+                //     }
+                //     if( (priority_projects.arduinoprojects.test(repo1.name) || /C\+\+/.test(repo1.language) {
+
+
+    
+                //     }
+
+                // }) ;                      
+
+
+                for (let i = 0 ; i < response.data.length ; i++){
+                    projectList_array.push(<IndivProject language={response.data[i].language} 
+                    name={response.data[i].name} 
+                    description={response.data[i].description} 
+                    svn_url={response.data[i].svn_url}  
+                    /> ) ; 
+                }
+                //JSX object containing all instance of IndivProject
+                return (<div> {projectList_array} </div>)
+                }
+                return (<div>Default message before request is made.</div>)
+                }}
+        </Get>
+        );
 
 }
 
